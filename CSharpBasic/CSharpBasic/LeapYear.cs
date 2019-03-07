42fd5621c761b2846b3a3280d25684167fe9a83f
HuXiaoPan <hupan008@qq.com>
2019-03-07 12:29:01 +0800

    闰闰年判断优化

--- CSharpBasic/CSharpBasic/LeapYear.cs
+++ CSharpBasic/CSharpBasic/LeapYear.cs
@@ -13,11 +13,16 @@ namespace CSharpBasic
     {
         static void Main(string[] args)
         {
+            /*
+             * 这里缺一个年份的健壮性判断
+             * 和一个循环查询和退出的菜单
+             * 懒得写over
+             */
             Console.WriteLine("请输入年份");
             string s = Console.ReadLine();
             int year = int.Parse(s);
             bool result = (year % 100 != 0 && year % 4 == 0)||(year%400==0);
-            Console.WriteLine(year + "年是闰年？" + result);
+            Console.WriteLine(year + "年"+ (result ? "是" : "不是") + "闰年");
         }
     }
 }
